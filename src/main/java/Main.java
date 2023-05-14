e2ecd3e75a3f4f6f831c77ba5c284a6e246d49a2
s3953344 <s3953344@student.rmit.edu.au>
2023-05-14 13:42:19 +1000

    changed import in main to model.*

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
 package src.main.java;
 
 import java.util.Scanner;
-
-import src.main.java.model.FinanceManager;
-import src.main.java.model.GeneralUser;
-import src.main.java.model.Manager;
-import src.main.java.model.OnlineSystem;
-import src.main.java.model.RefundRequest;
-import src.main.java.model.Trainee;
-import src.main.java.model.TrainingCourse;
-
 import java.util.ArrayList;   
+
+import src.main.java.model.*;
 
 public class Main {
 
